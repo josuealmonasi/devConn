c6622bc1f98fcdbd313606f6ed28852c3bbc4a3b
JosueIvn <josue.almonasi@hotmail.com>
2018-09-11 14:27:44 -0500

    JWT generated, and login improvements

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://josue:w132WQa1@ds251632.mlab.com:51632/josuealmonasidevconnector'
+  mongoURI: 'mongodb://josue:w132WQa1@ds251632.mlab.com:51632/josuealmonasidevconnector',
+  secretOrKey: 'secret'
 }
