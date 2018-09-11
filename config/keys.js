4d890950714c52561d358fbdab3d1033e8bf7674
Josue <josue.almonasi@hotmail.com>
2018-09-11 02:21:49 -0500

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://josue:w132WQa1@ds251632.mlab.com:51632/josuealmonasidevconnector'
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
+// Init app
+const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Coonect to mongo db
+mongoose
+  .connect(db)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
