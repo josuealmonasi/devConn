196f67566fe5c0870e449a4ebbdef5820a5274cd
Josue <josue.almonasi@hotmail.com>
2018-09-11 23:01:46 -0500

    Validator added

---
+++ validation/register.js
@@ -0,0 +1,26 @@
+const isEmpty = require('./is-empty');
+const Validator = require('validator');
+
+const validateRegisterInput = data => {
+  let errors = {};
+  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = 'Name must be between 2 and 30 characters';
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
+
+// module.exports = function validateRegisterInput(data) {
+//   let errors = {};
+//   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+//     errors.name = 'Name must be between 2 and 30 characters';
+//   }
+//   return {
+//     errors,
+//     isValid: isEmpty(errors)
+//   };
+// };
+
+module.exports = validateRegisterInput;
