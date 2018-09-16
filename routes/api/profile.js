04e173f12404cc02ee4c32c87013defc1f8750fb
Josue <josue.almonasi@hotmail.com>
2018-09-15 19:08:52 -0500

    Get profile route  added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,40 @@
 const express = require('express');
+const moongoose = require('mongoose');
+const passport = require('passport');
 const router = express.Router();
+
+// Load profile model
+const Profile = require('../../models/Profile');
+// Load user model
+const User = require('../../models/User');
 
 // @route   GET api/profile/test
 // @desc    Test profile route
 // @access  Public
 router.get('/test', (req, res) => {
-  res.json({
-    msg: 'Profile Works'
-  })
+  res.json({ msg: 'Profile Works' });
 });
+
+// @route   GET api/profile
+// @desc    Get current user profile
+// @access  Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'No profile for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  }
+);
 
 module.exports = router;
