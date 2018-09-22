b7ffc5b8f183ac689729ed04c46266170288e631
Josue <josue.almonasi@hotmail.com>
2018-09-22 02:05:59 -0500

    Post and delete comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -141,4 +141,64 @@ router.post(
   }
 );
 
+// @route   POST api/posts/comment/:id
+// @desc    Comment post
+// @access  Private
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+        post.comments.unshift(newComment);
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'Post not found' }));
+  }
+);
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Remove comment from post
+// @access  Private
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // Get removeIndex
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+        // Check if comment exists
+        if (removeIndex < 0) {
+          return res
+            .status(404)
+            .json({ commentnotfound: 'Comment does not exist' });
+        }
+        // Check for user
+        if (post.comments[removeIndex].user.toString() !== req.user.id) {
+          return res.status(401).json({
+            unauthorized: 'You can only remove comments created by you'
+          });
+        }
+        // Splice
+        post.comments.splice(removeIndex, 1);
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'Post not found' }));
+  }
+);
+
 module.exports = router;
