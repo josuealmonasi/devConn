27d4365089f20ea2d4be9ce86e1db16cbb252dd1
josuealmonasi <josue.almonasi@hotmail.com>
2018-10-17 16:47:36 -0500

    Login action and SET_CURRENT_USER

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,6 @@
+import { SET_CURRENT_USER } from '../actions/types';
+import isEmpty from '../validation/is-empty';
+
 const initialState = {
   isAutenticated: false,
   user: {}
@@ -5,6 +8,12 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAutenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
     default:
       return state;
   }
