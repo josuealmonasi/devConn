30155226e692b19e33137cf58896a867b630aab5
Josue Almonasi <josue.almonasi@arrow.com>
2019-05-14 13:15:25 -0500

    Redirect to login page  when logged out user tries to access using URL

--- client/src/App.js
+++ client/src/App.js
@@ -21,48 +21,52 @@ import './App.css';
 
 // Check for token
 if (localStorage.jwtToken) {
-	// Set auth token header auth
-	setAuthToken(localStorage.jwtToken);
-	// Decode token and get user info and exp
-	const decoded = jwt_decode(localStorage.jwtToken);
-	// Set user and isAuthenticated
-	store.dispatch(setCurrentUser(decoded));
-	// Check for expired token
-	const currentTime = Date.now() / 1000;
-	if (decoded.exp < currentTime) {
-		// Logout user
-		store.dispatch(logOutUser());
-		// Clear current Profile
-		store.dispatch(clearProfile());
-		// Redirect to login
-		window.location.href = '/login';
-	}
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logOutUser());
+    // Clear current Profile
+    store.dispatch(clearProfile());
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
-	render() {
-		return (
-			<Provider store={store}>
-				<Router>
-					<div className="App">
-						<Navbar />
-						<Route exact path="/" component={Landing} />
-						<div className="container">
-							<Route exact path="/register" component={Register} />
-							<Route exact path="/login" component={Login} />
-							<Switch>
-								<PrivateRoute exact path="/dashboard" component={Dashboard} />
-							</Switch>
-							<Switch>
-								<CreateProfile exact path="/create-profile" component={CreateProfile} />
-							</Switch>
-						</div>
-						<Footer />
-					</div>
-				</Router>
-			</Provider>
-		);
-	}
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
+              </Switch>
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
